4a865ee80ea4298b7b067168f4a3b51dfcd0ca82
Siora van Weperen <428439@student.fontys.nl>
2022-03-21 14:43:31 +0100

    DC-1 create project

---
+++ dj-collab/src/main/java/nl/fontys/djcollab/DjCollabApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.djcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjCollabApplication.class, args);
+	}
+
+}
