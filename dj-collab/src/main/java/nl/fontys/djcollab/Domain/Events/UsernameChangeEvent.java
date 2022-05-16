8509213b45c0ba3ce2f4beedd4ca8f1dbbd83aeb
Siora van Weperen <428439@student.fontys.nl>
2022-05-16 12:04:58 +0200

    Update UsernameChangeEvent.java

--- dj-collab/src/main/java/nl/fontys/djcollab/Domain/Events/UsernameChangeEvent.java
+++ dj-collab/src/main/java/nl/fontys/djcollab/Domain/Events/UsernameChangeEvent.java
@@ -3,9 +3,11 @@ package nl.fontys.djcollab.Domain.Events;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class UsernameChangeEvent {
-    private String userId;
+    private UUID userId;
     private String username;
 }
